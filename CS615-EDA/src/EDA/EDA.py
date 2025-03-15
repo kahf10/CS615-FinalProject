68aaca2182fd99abb86799cb4f1e8647e8010e3f
Kahf Hussain <kahfnhussain@gmail.com>
2025-03-15 13:31:01 -0400

    Just added project structure

--- CS615-EDA/src/EDA/EDA.py
+++ CS615-EDA/src/EDA/EDA.py
@@ -19,15 +19,15 @@ def main():
     pd.set_option('display.max_columns', None)
     eda = EDA()
 
-    understandDatasetSummaryForEachYear(eda, 2021, '../data/TOS Kaggle data week ending 2021 01 01.csv')
+    understandDatasetSummaryForEachYear(eda, 2021, '../../data/TOS Kaggle data week ending 2021 01 01.csv')
 
-    understandDatasetSummaryForEachYear(eda, 2022, '../data/TOS Kaggle data week ending 2022 01 07.csv')
+    understandDatasetSummaryForEachYear(eda, 2022, '../../data/TOS Kaggle data week ending 2022 01 07.csv')
 
-    understandDatasetSummaryForEachYear(eda, 2023, '../data/TOS Kaggle data week ending 2023 01 06.csv')
+    understandDatasetSummaryForEachYear(eda, 2023, '../../data/TOS Kaggle data week ending 2023 01 06.csv')
 
-    understandDatasetSummaryForEachYear(eda, 2024, '../data/TOS Kaggle data week ending 2024 01 05.csv')
+    understandDatasetSummaryForEachYear(eda, 2024, '../../data/TOS Kaggle data week ending 2024 01 05.csv')
 
-    understandDatasetSummaryForEachYear(eda, 2025, '../data/TOS Kaggle data week ending 2025 01 03.csv')
+    understandDatasetSummaryForEachYear(eda, 2025, '../../data/TOS Kaggle data week ending 2025 01 03.csv')
 
 
 def understandDatasetSummaryForEachYear(eda, year, filepath):
